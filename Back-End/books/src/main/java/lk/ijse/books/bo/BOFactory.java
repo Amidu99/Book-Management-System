ca1dbb8c25d7fa1da010b2090088db86d7f45b51
Amidu99 <amidushamika@gmail.com>
2023-12-28 11:22:12 +0530

    finalize the BOFactory

--- Back-End/books/src/main/java/lk/ijse/books/bo/BOFactory.java
+++ Back-End/books/src/main/java/lk/ijse/books/bo/BOFactory.java
@@ -1,5 +1,24 @@
 package lk.ijse.books.bo;
 
-public class BOFactory {
+import lk.ijse.books.bo.custom.impl.BookBOImpl;
 
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {}
+
+    public static BOFactory getBOFactory() {
+        return boFactory == null ? boFactory = new BOFactory():boFactory;
+    }
+
+    public enum BOTypes {
+        BOOK
+    }
+
+    public SuperBO getBO(BOTypes boTypes) {
+        switch (boTypes) {
+            case BOOK: return new BookBOImpl();
+            default: return null;
+        }
+    }
 }
